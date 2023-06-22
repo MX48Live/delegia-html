059d9f8325e70c647c4363ad12279c2967c8fe86
Watee K <Watee.K@link-th.com>
2023-06-22 19:10:32 +0700

    Add Hero

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
-const mobileToggle = document.querySelector('.mobile-toggle');
-const mobileMenu = document.querySelector('nav');
-mobileToggle.addEventListener('click', () => { 
-  mobileMenu.classList.toggle('open');
-});
+$(document).ready(function () { 
+  $('#slick-section').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
 
-const featureMobileToggle = document.querySelector('.feature-mobile-toggle');
-const featureMobile = document.querySelector('footer .feature');
-featureMobileToggle.addEventListener('click', () => { 
-  featureMobile.classList.toggle('open');
-});
+  $('.mobile-toggle').click(function () {
+    $('nav').toggleClass('open');
+  })
+  
+  $('.feature-mobile-toggle').click(function () {
+    $('footer .feature').toggleClass('open');
+  })
+})
