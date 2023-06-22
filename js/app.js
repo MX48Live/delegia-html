72b8c8c648a5985ade3a32fa8e81d45daf090781
Watee K <Watee.K@link-th.com>
2023-06-22 14:45:30 +0700

    Add Footer

--- js/app.js
+++ js/app.js
@@ -3,3 +3,9 @@ const mobileMenu = document.querySelector('nav');
 mobileToggle.addEventListener('click', () => { 
   mobileMenu.classList.toggle('open');
 });
+
+const featureMobileToggle = document.querySelector('.feature-mobile-toggle');
+const featureMobile = document.querySelector('footer .feature');
+featureMobileToggle.addEventListener('click', () => { 
+  featureMobile.classList.toggle('open');
+});
